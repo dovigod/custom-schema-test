75088ee712b33fec6219a4b489f919f93688cc2d
dovigod <inaki96@naver.com>
2024-04-29 11:21:07 +0900

    adw

---
+++ src/app/testing/page.jsx
@@ -0,0 +1,70 @@
+export default function FF() {
+  return (
+    <>
+      <div
+        onClick={async () => {
+          const res = await fetch("https://gerioernrongoer.regoiremg.com");
+          console.log(res);
+        }}
+      >
+        hiiiii!!!!!!
+      </div>
+      <button
+        style={{ padding: "20px", backgroundColor: "tomato" }}
+        onClick={() => {
+          window.open("https://xclusive.market/?openExternalBrowser=1");
+        }}
+      >
+        click me!! ( ios + android , line)
+      </button>
+
+      <button
+        style={{ padding: "20px", backgroundColor: "tomato" }}
+        onClick={() => {
+          window.open(
+            `kakaotalk://web/openExternal?url=${encodeURIComponent(
+              "https://xclusive.market"
+            )}`
+          );
+        }}
+      >
+        click me!! ( ios + android , kakao)
+      </button>
+
+      <button
+        style={{ padding: "20px", backgroundColor: "tomato" }}
+        onClick={() => {
+          window.open(
+            `intent://${encodeURIComponent(
+              "https://xclusive.market"
+            )}#Intent;scheme=http;package=com.android.chrome;end`
+          );
+        }}
+      >
+        click me!! ( android default browser- chrome)
+      </button>
+
+      <button
+        style={{ padding: "20px", backgroundColor: "tomato" }}
+        onClick={() => {
+          window.open(
+            `googlechrome://${encodeURIComponent("https://xclusive.market")}`
+          );
+        }}
+      >
+        click me!! ( ios browser- chrome)
+      </button>
+
+      <button
+        style={{ padding: "20px", backgroundColor: "tomato" }}
+        onClick={() => {
+          window.open(
+            `x-web-search://?${encodeURIComponent("xclusive.market")}`
+          );
+        }}
+      >
+        click me!! ( ios default browser - safari )
+      </button>
+    </>
+  );
+}
